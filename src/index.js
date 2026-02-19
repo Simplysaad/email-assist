74564488026f9760e63de8c191df840e83c36e58
simplysaad <saadidris70@gmail.com>
2026-02-19 14:39:12 +0100

    feat: create nodemailer utility function

---
+++ src/index.js
@@ -0,0 +1,35 @@
+﻿import express from "express";
+import "dotenv/config";
+import { sendEmail } from "./utils/nodemailer.util.js";
+// const app = express();
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`connected to PORT ${process.env.PORT}`);
+// });
+
+// app.get("/api/send-email", async (req, res, next) => {
+//   try {
+//     const { emailAddress, template, data } = req.body;
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// app.get("/api/broadcast", async (req, res, next) => {
+//   try {
+//     const { emailAddresses, template, data } = req.body;
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+sendEmail({
+  to: "saadidris23@gmail.com",
+  from: "saad <saadidris70@gmail.com>",
+  subject: "Test",
+  // html: "this is a test",
+  html: "<strong>This is a strong text</strong>"
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
---
+++ src/utils/nodemailer.util.js
@@ -0,0 +1,34 @@
+﻿import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  requireTLS: process.env.SMTP_HOST === "smtp.gmail.com" ? true : undefined,
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS
+  }
+});
+
+const sampleUser = {
+  name: "saad idris",
+  email: "saadidris23@gmail.com"
+};
+
+export async function sendEmail(emailOptions) {
+  const { to, subject, text, html } = emailOptions;
+
+  try {
+    await transporter.sendMail({
+      from: "saad <saadidris70@gmail.com>",
+      to,
+      subject,
+      text,
+      html
+    });
+  } catch (err) {
+    console.error("Error sending email:", err);
+    throw err;
+  }
+}
