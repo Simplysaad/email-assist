f2ee1513db8b3643d97713fcd6ec4187b3b5caa7
Saad Idris <saadidris70@gmail.com>
2026-02-21 08:33:43 +0100

    fix index.js,

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors"
-import errorMiddleware from "error.middleware.js"
+import errorMiddleware from "./error.middleware.js"
 import {
   sendEmail
 } from "./utils/nodemailer.util.js";
