78fb2d7ce195c76ac50083bc6b6ec33fafd176a2
Saad Idris <saadidris70@gmail.com>
2026-02-21 08:25:24 +0100

    Update index.js, extended error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-﻿import express from "express";
+import express from "express";
 import "dotenv/config";
 import cors from "cors"
 import errorMiddleware from "error.middleware.js"
-import { sendEmail } from "./utils/nodemailer.util.js";
+import {
+  sendEmail
+} from "./utils/nodemailer.util.js";
 import Template from "./models/template.model.js";
 import connectDB from "./utils/db.js";
 import handlebarsToHtml from "./utils/handlebars.util.js";
@@ -10,7 +12,9 @@ import keepAlive from "./utils/keep-alive.js";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.listen(process.env.PORT, () => {
   console.log(`connected to PORT ${process.env.PORT}`);
@@ -21,7 +25,7 @@ keepAlive()
 
 app.use(cors())
 app.use(errorMiddleware())
-app.get("/", (req, res)=>{
+app.get("/", (req, res)=> {
   return res.status(200).json({
     success: true,
     messages: `Today is ${new Date().toLocaleString()}, API is up and running`
@@ -29,13 +33,13 @@ app.get("/", (req, res)=>{
 })
 
 app.post("/api/template", async (req, res, next) => {
+  const {
+    title, subject, body, description, params = []
+  } = req.body;
+  const random = Math.floor(Math.random() * 1000);
+
   try {
-    const { title, subject, body, description, params = [] } = req.body;
-
-    // FIX: Math.random is a function, it needs ()
-    const random = Math.floor(Math.random() * 1000);
-
-    const newTemplate = new Template({
+    const newTemplate = new Template( {
       subject,
       body,
       description,
@@ -47,40 +51,65 @@ app.post("/api/template", async (req, res, next) => {
 
     await newTemplate.save();
 
-    return res.status(201).json({
-      success: true,
-      message: `New template created successfully`,
-      data: newTemplate
-    });
   } catch (err) {
-    next(err);
+    // next(err);
+    return res.status(400).json({
+      success: false,
+      message: `Error encountered while creating new template`,
+      error: err
+    });
   }
+  return res.status(201).json({
+    success: true,
+    message: `New template created successfully`,
+    data: newTemplate
+  });
 });
 
 app.post("/api/send-email", async (req, res, next) => {
+
+  const {
+    to, text, subject, templateId, templateParams
+  } = req.body;
+
   try {
-    const { to, text, subject, templateId, templateParams } = req.body;
 
     const currentTemplate = await Template.findById(templateId);
     if (!currentTemplate)
-      return res.status(404).json({ message: "Template not found" });
+      return res.status(404).json({
+      success: false, message: "Template not found"
+    });
+  }
 
-    const html = handlebarsToHtml(currentTemplate.body, templateParams);
+  catch(err) {
+    return res.status(500).json({
+      success: false, message: "Error encountered while finding template"
+    });
+  }
 
-    const email = await sendEmail({
+  const html = handlebarsToHtml(currentTemplate.body, templateParams);
+
+  try {
+    const email = await sendEmail( {
       to,
       from: "suhoorgroup <suhoorapp@gmail.com>",
-      subject: subject || currentTemplate.subject, 
+      subject: subject || currentTemplate.subject,
       text,
       html
     });
-
-    return res.status(200).json({
-      success: true,
-      message: `Mail sent to ${to} successfully`,
-      data: email
-    });
   } catch (err) {
-    next(err);
+    // next(err);
+
+    return res.status(500).json({
+      success: false,
+      message: "Error encountered while sending email",
+      error: err
+    })
   }
+  return res.status(200).json({
+    success: true,
+    message: `Mail sent to ${to} successfully`,
+    data: email
+  });
+
 });
