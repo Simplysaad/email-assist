468d2080a4f8cd78389445830c13d758ffcef1a6
Saad Idris <saadidris70@gmail.com>
2026-02-21 08:44:55 +0100

    fix index.js

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.listen(process.env.PORT, () => {
 keepAlive()
 
 app.use(cors())
-app.use(errorMiddleware())
+app.use(errorMiddleware)
 app.get("/", (req, res)=> {
   return res.status(200).json({
     success: true,
