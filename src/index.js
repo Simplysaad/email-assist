9a7c2d34707460335f505780825f943b497dbb6f
Saad Idris <saadidris70@gmail.com>
2026-02-21 07:56:47 +0100

    Update index.js, add error.middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 ﻿import express from "express";
 import "dotenv/config";
 import cors from "cors"
+import errorMiddleware from "error.middleware.js"
 import { sendEmail } from "./utils/nodemailer.util.js";
 import Template from "./models/template.model.js";
 import connectDB from "./utils/db.js";
@@ -19,7 +20,7 @@ app.listen(process.env.PORT, () => {
 keepAlive()
 
 app.use(cors())
-
+app.use(errorMiddleware())
 app.get("/", (req, res)=>{
   return res.status(200).json({
     success: true,
