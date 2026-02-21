16839a95816f6c9d5a1d540b4e86882b5160e516
Saad Idris <saadidris70@gmail.com>
2026-02-21 07:55:27 +0100

    Create error.middleware.js

---
+++ src/error.middleware.js
@@ -0,0 +1,25 @@
+export default function errorMiddleware(err, req, res, next) {
+  console.error("Error:", err);
+
+  const status = err.statusCode || err.status || 500;
+
+  const response = {
+    status: "error",
+    success: false,
+    message: err.message || "Internal Server Error"
+  };
+
+  if (err.name === "JsonWebTokenError") {
+    response.message = "invalid token";
+  }
+  if (err.name === "TokenExpiredError") {
+    response.message = "token expired";
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    response.stack = err.stack;
+  }
+
+  console.error(err);
+  return res.status(status).json(response);
+}
